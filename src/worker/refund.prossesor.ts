6c25d3f9c5726d141313fb84f72280e1ea4515ba
vuongps38770 <166083538+vuongps38770@users.noreply.github.com>
2025-07-10 20:22:19 +0700

    Add refund processor worker for handling refunds
    Introduces RefundProcessor to handle refund jobs from the REFUND_QUEUE using Bull and NestJS. The processor calls PaymentService to process refunds and logs the outcome for each order.

---
+++ src/worker/refund.prossesor.ts
@@ -0,0 +1,27 @@
+import { Processor, Process, InjectQueue } from "@nestjs/bull";
+import { Job, Queue } from "bull";
+import { Type } from "class-transformer";
+import { Types } from "mongoose";
+import { PaymentService } from "src/api/payment/payment.service";
+import { RedisJobName, RedisQueueName } from "src/redis/constants/redis-queue.constant";
+
+@Processor(RedisQueueName.REFUND_QUEUE)
+export class RefundProcessor {
+    constructor(
+        private readonly paymentService: PaymentService,
+    ) { }
+
+    @Process(RedisJobName.REFUND_JOB)
+    async handleRefund(job: Job<{ paymentId: string, orderId: string }>) {
+
+        const { orderId, paymentId } = job.data;
+        try {
+            console.log(` Bắt đầu hoàn tiền cho order ${orderId}`);
+            const refund = await this.paymentService.refundPayment(new Types.ObjectId(paymentId), "Hoàn tiền đơn hàng")
+            console.log(`✅ Hoàn tiền thành công cho order ${orderId}`);
+        } catch (err) {
+            console.error(`❌ Refund thất bại cho order ${orderId}:`, err.message);
+            throw err;
+        }
+    }
+}
